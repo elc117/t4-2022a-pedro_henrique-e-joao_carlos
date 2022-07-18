c70430466dfdb3e31ce1c9138fd0f85fc301b74f
Pedro Henrique da Silva Hinerasky <104012810+P3droSilva@users.noreply.github.com>
2022-07-18 19:38:05 -0300

    Add files via upload

---
+++ Trabalho Final/html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.mygdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                //return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                return new GwtApplicationConfiguration(1900, 1069);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
