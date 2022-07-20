7fd69276279e29ca9ac8db7d690b2841b947df67
P3dro_Silva <phhinerasky@inf.ufsm.br>
2022-07-20 16:19:26 -0300

    update

--- t4-2022a-pedro_henrique-e-joao_carlos/core/src/com/badlogic/game/GameScreen.java
+++ t4-2022a-pedro_henrique-e-joao_carlos/core/src/com/badlogic/game/GameScreen.java
@@ -3,8 +3,8 @@ package com.badlogic.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
 
@@ -13,21 +13,34 @@ public class GameScreen implements Screen {
     final ProtectManjaro game;
     static private int WIDTH = 1200;
     static private int HEIGHT = 675;
-    OrthographicCamera camera;
     Texture game_background;
     Texture castelo1;
-    Texture knight;
+    Texture knight_img;
+    Texture dragon_img;
+    Rectangle knight;
+    Rectangle dragon;
 
     public GameScreen(final ProtectManjaro passed_game) {
         game = passed_game;
 
         game_background = new Texture(Gdx.files.internal("game_background.png"));
         castelo1 = new Texture(Gdx.files.internal("Castle1.png"));
-        knight = new Texture(Gdx.files.internal("knight.png"));
+        knight_img = new Texture(Gdx.files.internal("knight.png"));
+        dragon_img = new Texture(Gdx.files.internal("red_dragon1.png"));
 
+        knight = new Rectangle();
+        knight.x = 900;
+        knight.y = 300;
+        knight.height = 64;
+        knight.width = 64;
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, WIDTH, HEIGHT);
+        dragon = new Rectangle();
+        dragon.height = 128;
+        dragon.width = 144;
+        dragon.x = 10;
+        dragon.y = 250;
+
+        game.camera.setToOrtho(false, WIDTH, HEIGHT);
     }
 
     @Override
@@ -35,14 +48,41 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+        game.camera.update();
+        game.batch.setProjectionMatrix(game.camera.combined);
         game.batch.begin();
         game.batch.draw(game_background, 0, 0);
         game.batch.draw(castelo1, 1000, 280);
         game.batch.draw(castelo1, 1000, 100);
-        game.batch.draw(knight, 900, 150);
+        game.batch.draw(knight_img, knight.x, knight.y);
+        game.batch.draw(dragon_img, dragon.x, dragon.y);
         game.batch.end();
+
+        if(Gdx.input.isKeyPressed(Keys.UP) || Gdx.input.isKeyPressed(Keys.W)) {
+            knight.y += 250 * Gdx.graphics.getDeltaTime();;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S)) {
+            knight.y -= 250 * Gdx.graphics.getDeltaTime();;
+        }
+        if(Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A)) {
+            knight.x -= 250 * Gdx.graphics.getDeltaTime();;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D)) {
+            knight.x += 250 * Gdx.graphics.getDeltaTime();;
+        }
+
+        if(knight.y > 400){
+            knight.y = 400;
+        }
+        if(knight.y < 100){
+            knight.y = 100;
+        }
+        if(knight.x > 900){
+            knight.x = 900;
+        }
+        if(knight.x < 200){
+            knight.x = 200;
+        }
     }
 
     @Override
